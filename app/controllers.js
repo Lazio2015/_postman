9a1932598787238aee694bc64efbdc5da0fcc36d
Lenur Memetov <Lenur.m@wecandevelopit.com>
2015-11-05 18:20:32 +0300

    rootScope

--- app/controllers.js
+++ app/controllers.js
@@ -6,14 +6,14 @@ postman.controller('RestCtrl', ['$scope','$http', 'HistoryService', '$rootScope'
         $scope.title = 'Contact page';
     }])
 
-    .controller('HistoryCtrl', ['$scope', 'HistoryService', '$rootScope', function($scope, HistoryService, $rootScope) {
+    .controller('HistoryCtrl', ['$scope', 'HistoryService', '$rootScope', function($scope, HistoryService) {
         $scope.title = 'History page';
         HistoryService.getlist().success(function(cb) {
             $scope.history = cb;
         });
 
-        $rootScope.$on('history:add', function(event, obj) {
-            $rootScope.history.push(obj);
+        $scope.$on('history:add', function(event, obj) {
+            $scope.history.push(obj);
         });
 
         $scope.$on('history-delete', function(event, data) {
