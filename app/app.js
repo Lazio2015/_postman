3d5eea60232ecc0ac4f472ba773ae5a9d3faecfb
Lazio <lenur.m@wecandevelopit.com>
2015-11-08 20:28:01 +0300

    update

--- app/app.js
+++ app/app.js
@@ -1,31 +1,27 @@
 'use strict';
 
-var postman = angular.module('App', ['ngRoute', 'ngResource']).config(function($routeProvider){
+var postman = angular.module('App', ['ngRoute', 'ngResource'])
+    .config(['$routeProvider', function($routeProvider){
+        //$locationProvider.html5Mode({
+        //    enabled: true,
+        //    requireBase: false
+        //});
         $routeProvider.when('/contact', {
             templateUrl: 'app/contact/index.html',
             controller: 'ContactCrtl'
-        }); //'App.history' - CONNECT ng-app
+        });
         $routeProvider.when('/rest', {
             templateUrl: 'app/rest/index.html',
             controller: 'RestCtrl'
         });
         $routeProvider.otherwise({redirectTo: '/'});
-    })
+    }])
 
     .constant('SystemConfig', {
         baseUrl: 'http://localhost:3000/server',
         historyUrl: 'http://localhost:3000/history',
         flag: true
     })
-
-    .directive('delete',['HistoryService', function(HistoryService){
-        return function($scope, element, attrs){
-            element.on('click', function(){
-                HistoryService.delete(attrs.id);
-            });
-        };
-    }])
-
 
     .directive('sendDataRest',['$http', '$rootScope', 'SystemConfig', function($http, $rootScope, SystemConfig){
         return {
@@ -36,71 +32,52 @@ var postman = angular.module('App', ['ngRoute', 'ngResource']).config(function($
             },
             controller: function($scope) {
                 $scope.title = 'Rest: SendData page';
+                $scope.data = {
+                    type: 'GET',
+                    url: ''
+                };
 
-                //$rootScope.$on('main:response', function (e, res, history) {
-                //    $scope.response = res;
-                //    history.id = res.id;
-                //    $scope.$broadcast('history:add', history);
-                //});
-
-                $scope.$on('addUrl', function(e, query) {
-                    console.log('2');
-                    query.url = jQuery(element).find('#url').val(query.url);
-                    query.type = jQuery(element).find('#type').val(query.type);
+                $scope.$on('sendData: invoke', function(e, query) {
+                    $scope.data = query;
                 });
             },
-        //    controller: 'SendDataCtrl',
             link: function(scope, element, attrs) {
                 var button = jQuery(element).find('#send');
 
+                // send data by click
                 button.on('click', function() {
-                    var query = {url: '', type: 'GET'};
-                    query.url = jQuery(element).find('#url').val();
-                    query.type = jQuery(element).find('#type').val();
+                    button.prop('disabled', true);
 
-                    $http.post(SystemConfig.baseUrl, query).success(function(data) {
-                        scope.$broadcast('send:response', data);
-                        scope.$broadcast('history:add', history);
+                    $http.post(SystemConfig.baseUrl, scope.data).success(function(data) {
+                        $rootScope.$broadcast('sendData: response', data.content);
+                        $rootScope.$broadcast('history: add', data.history);
+
+                        button.prop('disabled', false);
                     });
-
                 })
             }
         }
     }])
 
-    .directive('historyRest', ['$http', '$rootScope', function($http, $rootScope) {
+    .directive('historyRest', ['$http', '$rootScope', 'SystemConfig', function($http, $rootScope, SystemConfig) {
         return {
             templateUrl: 'app/rest/history/history.html',
             controller: 'HistoryCtrl',
             link: function(scope, element, attrs) {
-                    //    click: function(scope, element, attrs){
-                    element.on('click', function () {
-                        var query = {};
-                        query.type = jQuery(element).find('.type');
-                        query.url = jQuery(element).find('.url');
 
-                        scope.$broadcast('addUrl', {url:'s', type:'df'});
+                $http.get(SystemConfig.historyUrl).success(function(data) {
+                    scope.history = data;
+                });
 
+                scope.delete = function(id) {
+                    $http.delete(SystemConfig.historyUrl+'/'+id).success(function(data, status) {
+                        scope.$broadcast('history: delete', data);
                     });
+                };
+
+                scope.invokeClick = function(data) {
+                    scope.$broadcast('sendData: invoke', data);
+                }
         }
     }
-    }])
-
-    .factory('HistoryService',['SystemConfig', '$http', '$rootScope', function(SystemConfig, $http, $rootScope){
-        return {
-            getlist: function(){
-                return $http.get(SystemConfig.historyUrl);
-            },
-
-            delete: function(id){
-                $http.delete(SystemConfig.historyUrl+'/'+id).success(function(data, status) {
-                    $rootScope.$broadcast('history-delete', data);
-                });
-            },
-
-            add: function() {
-                $http.post(SystemConfig.historyUrl).success(function(data, status) {
-                })
-            }
-        }
     }]);
--- app/controllers.js
+++ app/controllers.js
@@ -1,47 +1,30 @@
-postman.controller('RestCtrl', ['$scope','$http', 'HistoryService', '$rootScope', function($scope, $http, HistoryService, $rootScope) {
-    $scope.title = 'Rest API';
+postman
+    .controller('RestCtrl', ['$scope', function($scope) {
+        $scope.title = 'Rest API';
 
-    $scope.$on('send:response', function(e, data) {
-        $scope.response = data;
-        console.log(2);
-    });
-
+        $scope.$on('sendData: response', function(e, data) {
+            $scope.response = data;
+        });
     }])
+
     .controller('ContactCrtl',['$scope', function($scope){
         $scope.title = 'Contact page';
     }])
 
-    .controller('HistoryCtrl', ['$scope', 'HistoryService', '$rootScope', function($scope, HistoryService) {
+    .controller('HistoryCtrl', ['$scope', function($scope) {
         $scope.title = 'Rest: History page';
 
-        HistoryService.getlist().success(function(cb) {
-            $scope.history = cb;
+        $scope.$on('history: add', function(event, data) {
+            $scope.history.push(data);
         });
 
-        $scope.$on('history:add', function(event, obj) {
-            $scope.history.push(obj);
-        });
-
-        $scope.$on('history-delete', function(event, data) {
+        $scope.$on('history: delete', function(event, data) {
             $scope.history = data;
         });
-
     }])
 
     .controller('SendDataCtrl', ['$scope', function($scope) {
-        //$scope.title = 'Rest: SendData page';
-        //
-        //$rootScope.$on('main:response', function (e, res, history) {
-        //    $scope.response = res;
-        //    history.id = res.id;
-        //    $scope.$broadcast('history:add', history);
-        //});
-        //
-        //$scope.$on('addUrl', function(e, query) {
-        //    console.log('2');
-        //    query.url = jQuery(element).find('#url').val(query.url);
-        //    query.type = jQuery(element).find('#type').val(query.type);
-        //});
+
     }]);
 
 
